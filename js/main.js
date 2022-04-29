dde93f9382ae3865fce9a3b4bb1579a494fc2ffa
David Alvarez <100007463.joan23@fje.edu>
2022-04-29 16:23:33 +0200

    first changes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 function parrafos() {
     var infonum = document.getElementById('infonum')
-    infonum.innerHTML = 'You entered a non binary digit (please enter only 0 or 1)'
+
     var infonum2 = document.getElementById('infonum2')
-    if (numero << 1) {
-        var infonum2 = 'Here is your decimal'
+    var numero = document.getElementById('numero').value
+    var calculando = document.getElementById('calculando')
+    calculando.innerHTML = parseInt(numero, 2);
+    var ultimodigito = numero.charAt(numero.length - 1)
+    if (infonum == 0 || 1) {}
+    if (ultimodigito == 0 || ultimodigito == 1) {
+        infonum2.innerHTML = 'Here is your decimal'
+    } else {
+        calculando.innerHTML = 'Waiting for a valid binary number...'
+    } else {
+        infonum.innerHTML = 'You entered a non binary digit (please enter only 0 or 1)'
     }
 }
